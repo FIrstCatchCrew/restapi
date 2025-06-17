47309dc32028d4f02f5001fa0504fadab13227f6
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-06-17 18:48:31 -0230

    Initialize Spring Boot project with basic configuration. Add `.gitignore`, `.gitattributes`, `pom.xml`, and initial source/test files. Include Maven Wrapper.

---
+++ src/main/java/com/example/firstcatch_api/FirstcatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstcatch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstcatch_api/FirstcatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstcatch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstcatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
