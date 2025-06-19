49178beb824f510ed00f0de18bf81555521b6f6b
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-06-19 21:04:17 -0230

    Extend `FisherProfile` entity with additional attributes and update service methods

--- src/main/java/com/firstcatchcrew/restapi/fisherProfile/FisherProfile.java
+++ src/main/java/com/firstcatchcrew/restapi/fisherProfile/FisherProfile.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Entity
 public class FisherProfile {
     @Id
+    @SequenceGenerator(name = "fisherProfile_sequence", sequenceName = "fisherProfile_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "fisherProfile_sequence")
     private Long id;
 
     @OneToOne
@@ -24,4 +26,40 @@ public class FisherProfile {
     @OneToMany(mappedBy = "fisher")
     private List<Catch> catches;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getFishingLicenseNumber() {
+        return fishingLicenseNumber;
+    }
+
+    public void setFishingLicenseNumber(String fishingLicenseNumber) {
+        this.fishingLicenseNumber = fishingLicenseNumber;
+    }
+
+    public Landing getDefaultLanding() {
+        return defaultLanding;
+    }
+
+    public void setDefaultLanding(Landing defaultLanding) {
+        this.defaultLanding = defaultLanding;
+    }
+
+    public List<Catch> getCatches() {
+        return catches;
+    }
+
+    public void setCatches(List<Catch> catches) {
+        this.catches = catches;
+    }
 }
