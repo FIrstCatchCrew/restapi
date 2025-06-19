a34f256acc7f2fbdb29e36411b7c62f26cfed058
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-06-19 13:18:29 -0230

    Implement basic CRUD operations for `FisherProfile` module

--- src/main/java/com/firstcatchcrew/restapi/fisherProfile/FisherProfileService.java
+++ src/main/java/com/firstcatchcrew/restapi/fisherProfile/FisherProfileService.java
@@ -1,4 +1,51 @@
 package com.firstcatchcrew.restapi.fisherProfile;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FisherProfileService {
+
+    private final FisherProfileRepository fisherRepository;
+
+    public FisherProfileService(FisherProfileRepository fisherRepository) {
+        this.fisherRepository = fisherRepository;
+    }
+
+//    public List<FisherProfile> getAllFishers() {
+//        return fisherRepository.findAll();
+//    }
+
+    public FisherProfile getFisherById(long id) {
+        Optional<FisherProfile> fisherOptional = fisherRepository.findById(id);
+
+        return fisherOptional.orElse(null);
+    }
+    
+
+    public FisherProfile createFisher(FisherProfile newFisher) {
+        return fisherRepository.save(newFisher);
+    }
+
+    public FisherProfile updateFisher(long id, FisherProfile updatedFisher) {
+        Optional<FisherProfile> fisherToUpdateOptional = fisherRepository.findById(id);
+
+        if (fisherToUpdateOptional.isPresent()) {
+            FisherProfile fisherToUpdate = fisherToUpdateOptional.get();
+
+            //fisherToUpdate.setUsername(updatedFisher.getUsername());;
+
+            return fisherRepository.save(fisherToUpdate);
+        }
+
+        return null;
+    }
+
+    public void deleteFisherById(long id) {
+        fisherRepository.deleteById(id);
+    }
+
+
 }
