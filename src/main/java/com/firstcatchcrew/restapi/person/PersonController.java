67a3fc40c25a8b6180ac41f4436cc608ab0f76b4
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-06-19 13:17:04 -0230

    Implement basic CRUD operations for `Person` module

--- src/main/java/com/firstcatchcrew/restapi/person/PersonController.java
+++ src/main/java/com/firstcatchcrew/restapi/person/PersonController.java
@@ -1,4 +1,11 @@
 package com.firstcatchcrew.restapi.person;
 
 public class PersonController {
+
+//    @GetMapping("/people")
+//    public List<PersonDTO> getPeople() {
+//        return personService.findAll().stream()
+//                .map(p -> new PersonDTO(p.getId(), p.getName(), p.getRole().getLabel()))
+//                .collect(Collectors.toList());
+//    }
 }
--- src/main/java/com/firstcatchcrew/restapi/person/PersonRepository.java
+++ src/main/java/com/firstcatchcrew/restapi/person/PersonRepository.java
@@ -1,4 +1,14 @@
 package com.firstcatchcrew.restapi.person;
 
-public interface PersonRepository {
+import com.firstcatchcrew.restapi.role.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    List<Person> findByRole(Role role);
+    List<Person> findAll();
 }
