dc910158d29f4e1196db1892938546ec0c60478f
DC Elliott <davecliveelliott@hotmail.com>
2025-06-20 16:23:21 -0230

    OrderController and connections in service updated.

--- src/main/java/com/firstcatchcrew/restapi/order/OrderRepository.java
+++ src/main/java/com/firstcatchcrew/restapi/order/OrderRepository.java
@@ -2,6 +2,7 @@ package com.firstcatchcrew.restapi.order;
 
 import com.firstcatchcrew.restapi.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
